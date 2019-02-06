6dadd310f4f2219b8e9ea10627caf13d377251f2
Raúl Ojeda <rauloojs@gmail.com>
2019-02-06 10:25:06 -0600

    feature(angular searchPage): Add bootstrap classes. Add total_count response to vm

--- angular-version/src/app/searchPage/searchPage.controller.js
+++ angular-version/src/app/searchPage/searchPage.controller.js
@@ -3,13 +3,15 @@ export class SearchPageController {
     'ngInject';
     this.search = '';
     this.items = [];
+    this.totalCount = 0;
     this.api = api;
   }
 
   onSubmit () {
     this.api.searchRepositories(this.search)
-    .then(({ items }) => {
+    .then(({ items, total_count }) => {
       this.items = items;
+      this.totalCount = total_count;
     });
   }
 }
