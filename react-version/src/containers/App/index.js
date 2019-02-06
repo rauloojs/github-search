afd7043d3968ed51f5c2463912493787db426ef2
Raúl Ojeda <rauloojs@gmail.com>
2019-02-05 22:25:09 -0600

    chore(containers App): Move App to containers folder

---
+++ react-version/src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchPage from '../SearchPage';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchPage />
+      </div>
+    );
+  }
+}
+
+export default App;
