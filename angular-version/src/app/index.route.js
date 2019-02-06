bcbbfbe6a9f6b7078bc3de1b8aa91c99864ff325
Raúl Ojeda <rauloojs@gmail.com>
2019-02-06 01:12:59 -0600

    feature(angular routes): Use SearchPage for index route

--- angular-version/src/app/index.route.js
+++ angular-version/src/app/index.route.js
@@ -2,9 +2,9 @@ export function routerConfig ($routeProvider) {
   'ngInject';
   $routeProvider
     .when('/', {
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
+      templateUrl: 'app/searchPage/searchPage.html',
+      controller: 'SearchPageController',
+      controllerAs: 'searchPage'
     })
     .otherwise({
       redirectTo: '/'
