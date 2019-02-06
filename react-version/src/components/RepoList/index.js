8ff1de192538665aed61a289b1c02e8d576f2fa5
Raúl Ojeda <rauloojs@gmail.com>
2019-02-06 10:18:55 -0600

    feature(react components RepoList): Add bootstrap classes. Make it list

--- react-version/src/components/RepoList/index.js
+++ react-version/src/components/RepoList/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RepoItem from '../RepoItem';
 
-const RepoList = ({ repos }) => {
+const RepoList = ({ repos, totalCount }) => {
   return (
-    repos.map((repo) => (
-      <RepoItem
-        key={repo.id}
-        repo={repo}
-      />
-    ))
+    <div>
+      <h4>{totalCount} results</h4>
+      <ul className="list-group">
+        {repos.map((repo) => (
+          <RepoItem
+            key={repo.id}
+            repo={repo}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
 RepoList.propTypes = {
   repos: PropTypes.array,
+  totalCount: PropTypes.number,
 };
 
 export default RepoList;
