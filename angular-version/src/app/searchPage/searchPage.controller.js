d6efb813a9659b8227fe1a2ad127059e65a3aa87
Raúl Ojeda <rauloojs@gmail.com>
2019-02-06 11:03:49 -0600

    feature(angular searchPage): Add page to vm. Add handler for load more action

--- angular-version/src/app/searchPage/searchPage.controller.js
+++ angular-version/src/app/searchPage/searchPage.controller.js
@@ -3,11 +3,26 @@ export class SearchPageController {
     'ngInject';
     this.search = '';
     this.items = [];
+    this.page = 1;
     this.totalCount = 0;
     this.api = api;
   }
 
+  onLoadMore () {
+    this.page += 1;
+
+    this.api.searchRepositories(this.search, this.page)
+    .then(({ items }) => {
+      this.items = [
+        ...this.items,
+        ...items
+      ];
+    });
+  }
+
   onSubmit () {
+    this.page = 1;
+
     this.api.searchRepositories(this.search)
     .then(({ items, total_count }) => {
       this.items = items;
