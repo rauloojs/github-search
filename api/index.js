e4237068b86bfc18685fb0eec8b1cfe3d8057db8
Raúl Ojeda <rauloojs@gmail.com>
2019-02-05 22:27:16 -0600

    feature(api): Add commmon api module

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const baseURL = 'https://api.github.com';
+
+export const searchRepositories = (q = '') => {
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+
+  return fetch(`${baseURL}/search/repositories?q=${q}`, options)
+    .then((response) => response.json());
+};
