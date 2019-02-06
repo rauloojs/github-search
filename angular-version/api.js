4e49c654fa5036d6e99c5b292ecbbf95149c40a1
Raúl Ojeda <rauloojs@gmail.com>
2019-02-06 11:02:31 -0600

    feature(angular api): Support pagination options

--- angular-version/api.js
+++ angular-version/api.js
@@ -1,12 +1,12 @@
 const baseURL = 'https://api.github.com';
 
-export const searchRepositories = (q = '') => {
+export const searchRepositories = (q = '', page = 1) => {
   const options = {
     headers: {
       'Content-Type': 'application/json'
     }
   };
 
-  return fetch(`${baseURL}/search/repositories?q=${q}`, options)
+  return fetch(`${baseURL}/search/repositories?q=${q}&page=${page}&per_page=10`, options)
     .then((response) => response.json());
 };
