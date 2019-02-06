d2894727396b49bb548a02f36d03af205f694394
Raúl Ojeda <rauloojs@gmail.com>
2019-02-05 22:26:38 -0600

    feature(containers SearchPage): Add container for Search view

---
+++ react-version/src/containers/SearchPage/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { searchRepositories } from '../../../../api';
+import SearchBar from '../../components/SearchBar';
+import RepoList from '../../components/RepoList';
+
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: '',
+      items: [],
+    };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSearchChange(event) {
+    this.setState({
+      search: event.target.value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    const { search } = this.state;
+    const response = await searchRepositories(search);
+
+    debugger
+    this.setState({
+      ...response,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          search={this.state.search}
+          onSearchChange={this.handleSearchChange}
+          onSubmit={this.handleSubmit}
+        />
+        <RepoList repos={this.state.items} />
+      </div>
+    );
+  }
+}
+
+export default SearchPage;
