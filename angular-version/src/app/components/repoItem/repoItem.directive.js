786dde5ec78d4867998fde98b20647702367297b
Raúl Ojeda <rauloojs@gmail.com>
2019-02-06 01:10:52 -0600

    feature(angular components RepoItem): Add directive to render repo item

---
+++ angular-version/src/app/components/repoItem/repoItem.directive.js
@@ -0,0 +1,22 @@
+export function RepoItemDirective() {
+  'ngInject';
+
+  let directive = {
+    restrict: 'E',
+    templateUrl: 'app/components/repoItem/repoItem.html',
+    scope: {
+        repo: '='
+    },
+    controller: RepoItemController,
+    controllerAs: 'vm',
+    bindToController: true
+  };
+
+  return directive;
+}
+
+class RepoItemController {
+  constructor () {
+    'ngInject';
+  }
+}
